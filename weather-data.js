22d0f10ca34ef4d79432b993baaa220cf5a34070
Ty <tyler.peterson@utah.edu>
2016-08-29 20:17:40 -0600

    fixed async issue causing variable in ip api to not be in scope

--- weather-data.js
+++ weather-data.js
@@ -1,5 +1,31 @@
-    function gettingJSON(){        
-        $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=London&APPID=98ee2d73f7eef59301620cf461192eb7",function(json){
-            document.write(JSON.stringify(json));
-        });
-    }
+$(document).ready(function() {	
+
+	//add current cities json info to html
+    $.getJSON(createAPIKeyWithCurrentLocation() ,function(weather){
+        $('#temp').text(JSON.stringify(weather)); 
+        $('#location').text(JSON.stringify(weather.main.temp_min));
+    });
+
+	function getLocationZip(){
+			$.ajax({
+			url: "http://ip-api.com/json/",
+			async: false,
+			dataType: 'json',
+			success: function(ip){
+				localZip = ip.zip;		
+			}
+		});
+		return localZip;
+	}
+
+	function createAPIKeyWithCurrentLocation(){
+		var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=";
+		var units = "&units=imperial";
+		var apiKey = "&APPID=98ee2d73f7eef59301620cf461192eb7";
+		//add together key
+		var apiUrlFull = apiUrl + getLocationZip() + units + apiKey;
+
+		return apiUrlFull;
+	}
+
+});
