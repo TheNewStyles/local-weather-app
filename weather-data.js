ee1ac1ff6594419066e3c2c6410d68501f0d7659
Ty <tyler.peterson@utah.edu>
2016-09-06 18:41:16 -0600

    add farhren to celsius conversion

--- weather-data.js
+++ weather-data.js
@@ -1,10 +1,57 @@
+////////////////// Nav Menu  //////////////////
+function openNav(){
+  document.getElementById("nav").style.height= "100%";
+}
+
+function closeNav(){
+  document.getElementById("nav").style.height = "0%";
+}
+
 $(document).ready(function() {	
 
 	//add current cities json info to html
     $.getJSON(createAPIKeyWithCurrentLocation() ,function(weather){
-        $('#temp').text(JSON.stringify(weather)); 
-        $('#location').text(JSON.stringify(weather.main.temp_min));
+
+    	// City Temp
+    	var cityTemp = JSON.stringify(weather.main.temp);
+    	var cityTempRounded = Math.floor(cityTemp);
+        $('#temp').text(cityTempRounded); 
+        // City Name
+        var cityName = JSON.stringify(weather.name);
+        var cityNameNoQuotes = cityName.replace(/\"/g, "");
+        $('#location').text(cityNameNoQuotes);
+        // Weather Description
+        var weatherDescription = JSON.stringify(weather.weather[0].description);
+        var weatherDescriptionNoQuotes = weatherDescription.replace(/\"/g, "");
+        $('#weatherDescription').text(weatherDescriptionNoQuotes);
+        // City Temp
+    	var windSpeed = JSON.stringify(weather.wind.speed);
+    	var windSpeedRounded = Math.floor(windSpeed);
+        $('#wind').text(windSpeedRounded + " mph"); 
+        // Humidity
+    	var humidity = JSON.stringify(weather.main.humidity);
+    	//var windSpeedRounded = Math.floor(windSpeed);
+        $('#humidity').text(humidity); 
+
+        document.getElementById("fahrenheit").addEventListener("click", addNewTemp);
+
+        function convertFarenToCelsius(tempInFaren){
+    		return (tempInFaren-32) * (5/9);
+    	}
+
+    	function addNewTemp(){
+    		$('#temp').text(convertFarenToCelsius(cityTempRounded));
+    	}
+    	
+
     });
+
+
+
+
+    function convertCelsiusToFaren(){
+
+    }
 
 	function getLocationZip(){
 			$.ajax({
